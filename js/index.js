2fdd76109e076221b5d08334ae560602838a4234
m-mraisi <mahmodmreasi@gmail.com>
2022-09-21 23:41:54 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,5 @@
 localStorage.clear()
 
-// const buyTicket = () => {
-//     // console.log(ticketType,typeIndex,ticketPrice);
-//     const ticket = {
-//         "ticketType":ticketType,
-//         "ticketPrice":parseInt(ticketPrice)
-//     }
-//     console.log(ticket)
-//     localStorage.clear()
-//     localStorage.setItem('ticket',JSON.stringify(ticket))
-//     window.location.href = "../ticket.html";
-//     console.log("button pressed");
-//     return false;
-// }
-
-
-
 document.querySelector("#btn-allAccess-buy").addEventListener("click",()=>{
     localStorage.clear()
     const ticket = {
@@ -27,6 +11,24 @@ document.querySelector("#btn-allAccess-buy").addEventListener("click",()=>{
     // in windows ../ticket.html does not work.
 })
 
-// document.querySelector("#btn-allAccess-buy").addEventListener("click",buyTicket())
-// document.querySelector('#btn-goldPass-buy').addEventListener('click',buyTicket("gold",1,690))
-// document.querySelector('#btn-VIP-buy').addEventListener('click',buyTicket("VIP",2,1250))
+document.querySelector("#btn-goldPass-buy").addEventListener("click",()=>{
+    localStorage.clear()
+    const ticket = {
+        "ticketType":2,
+        "ticketPrice":690
+    }
+    localStorage.setItem('ticket',JSON.stringify(ticket));
+    window.location.href = "/Project-G05/ticket.html";
+    // in windows ../ticket.html does not work.
+})
+
+document.querySelector("#btn-VIP-buy").addEventListener("click",()=>{
+    localStorage.clear()
+    const ticket = {
+        "ticketType":3,
+        "ticketPrice":1250
+    }
+    localStorage.setItem('ticket',JSON.stringify(ticket));
+    window.location.href = "/Project-G05/ticket.html";
+    // in windows ../ticket.html does not work.
+})
